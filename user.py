54c5ffa91d385153a0c4798cc4ecbd8d5268969c
Soko Paul <sopapaso73@gmail.com>
2018-07-24 00:20:52 +0300

    set up api folder with v1 folder in it and a tests folder

--- user.py
+++
@@ -1,9 +0,0 @@
-
-class User():
-    def __init__(self, name, phone_number, password):
-        self.name = name
-        self.phone_number = phone_number
-        self.password = password
-
-    def get_user(self):
-        return self

e7cbe67b2f16e91fca5a186495622db53e8a551e
Soko Paul <sopapaso73@gmail.com>
2018-08-01 04:41:02 +0300

    connect endpoints to database and edit connetion to database

--- database/connection.py
+++ database/connection.py
@@ -5,18 +5,11 @@ import os
 
 def connect():
 
-    # conn = psycopg2.connect(
-    #     database='mydiary',
-    #     user='postgres',
-    #     password='',
-    #     host='localhost',
-    #     port='5432')
-
     conn = psycopg2.connect(
         database='mydiary',
-        user='soko',
-        password='sokool',
+        user='postgres',
+        password='',
         host='localhost',
-        port='5434')
+        port='5432')
 
     return conn
